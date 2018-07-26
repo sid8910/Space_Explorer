80bcd96891e022b473bf4e1ee8010f812a3a2eb6
Shubham Baghe <31929036+sid8910@users.noreply.github.com>
2018-07-26 21:07:59 +0530

    Add files via upload

---
+++ Assets/Rocket.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    [SerializeField] float rcsThrust = 100f;
+    [SerializeField] float mainThrust = 50f;
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip death;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
+    Rigidbody rigidBody;
+    AudioSource audioSource;
+    enum State {Alive, Dying, Transcending}
+    State state = State.Alive;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(state == State.Alive)
+        {
+            RespondToThrustInput();
+            ResponToRotateInput();
+        }
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(state != State.Alive) { return; }
+        switch(collision.gameObject.tag)
+        {
+            case"Friendly":
+                print("Friendly Object");
+                break;
+            case "Finish":
+                StartSeccessSequence();
+                break;
+            default:
+                StartDeathSequence();
+                break;
+        }
+    }
+
+    private void StartDeathSequence()
+    {
+        state = State.Dying;
+        audioSource.Stop();
+        audioSource.PlayOneShot(death);
+        deathParticles.Play();
+        Invoke("LoadFirstLevel", levelLoadDelay);
+    }
+
+    private void StartSeccessSequence()
+    {
+        state = State.Transcending;
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void LoadNextLevel()
+    {
+        SceneManager.LoadScene(1);  //todo for allow for more than two levels
+    }
+
+    private void ResponToRotateInput()
+    {
+        rigidBody.freezeRotation = true;    // take manual control of rotation
+        float rotationThisFrame = rcsThrust  * Time.deltaTime;
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidBody.freezeRotation = false;    // resume physics control of rotation
+    }
+
+    private void RespondToThrustInput()
+    {
+        if (Input.GetKey(KeyCode.Space))          //can thrust while rotating
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audioSource.Stop();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+}
